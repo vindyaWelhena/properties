e5486cbf9616ff0b6f76f31fde34f910feabe05b
Vindiya <vindya.20232002@iit.ac.lk>
2026-01-04 17:59:53 +0530

    Add code to Favourites pagee

---
+++ src/components/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites() {
+  return (
+    <div className="favourites">
+      <h3>Favourites</h3>
+      <p>No favourites added yet</p>
+    </div>
+  );
+}
+
+export default Favourites;
